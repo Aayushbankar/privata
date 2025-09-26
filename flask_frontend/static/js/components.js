ce12a1304d93b59f50cdb7ad17a7a9d861da77cc
aayushbankar <aayushbankar42@gmail.com>
2025-09-26 11:25:57 +0530

    just clean up

--- flask_frontend/static/js/components.js
+++
@@ -1,8 +0,0 @@
-/* MOSDAC AI Help Bot - UI Components */
-/* Reusable UI components and widgets */
-
-// Simple placeholder for components
-console.log('Components.js loaded');
-
-// Export empty object for now
-window.Components = {};
